c9dcafd7d9815e218a5cb7085a517c473e55072f
Vikash Kumar <itscoolvikash@gmail.com>
2023-12-15 12:54:55 +0000

    changed

--- .devcontainer/01_basic/first.js
+++ .devcontainer/01_basic/first.js
@@ -1,1 +1,6 @@
 console.log("Hello JS");
+
+// we cannot use var becouse it create problems in block or funstion(variable value override)
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);      // object
