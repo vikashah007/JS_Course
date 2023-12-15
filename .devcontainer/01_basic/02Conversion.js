8ad9e49babaeb744a82a9efeb96086da0d8bb700
Vikash Kumar <itscoolvikash@gmail.com>
2023-12-15 13:17:29 +0000

    changed

---
+++ .devcontainer/01_basic/02Conversion.js
@@ -0,0 +1,45 @@
+//case 1
+let score="33"
+console.log("string")
+console.log(typeof score);
+
+let numberedValue=Number(score);
+
+console.log(numberedValue)
+console.log(typeof numberedValue);
+ 
+//case 2
+let score1="33abc"
+console.log("STRING")
+console.log(typeof score1);
+
+let numberedValue1=Number(score1);
+console.log(numberedValue1)
+console.log(typeof numberedValue1);
+
+//case 3
+let score2=undefined;
+console.log("UNDEFINED")
+console.log(typeof score2);
+
+let numberedValue2=Number(score2);
+console.log(numberedValue2)
+console.log(typeof numberedValue2);
+
+//case 4
+let score3=null
+console.log("NULL")
+console.log(typeof score3);
+
+let numberedValue3=Number(score3);
+console.log(numberedValue3)
+console.log(typeof numberedValue3);
+
+//case 5
+let score4=true
+console.log("TRUE")
+console.log(typeof score4);
+
+let numberedValue4=Number(score4);
+console.log(numberedValue4)
+console.log(typeof numberedValue4);
