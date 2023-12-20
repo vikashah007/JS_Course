641bc773fa350ef09d6fcf1e533d747d4b16cc88
Vikash Kumar <itscoolvikash@gmail.com>
2023-12-20 13:37:10 +0000

    Data Type Summary

---
+++ .devcontainer/01_basic/03DataTypes.js
@@ -0,0 +1,14 @@
+// Primitive Data Types:-
+// 7 Types:- String,Number,Boolean,null,undefined,Symbol
+
+//Reference (Non Primitive) :- Array, objects, function
+
+//Arrays Ex:-
+let arr=["vikash","asha","aka"]
+
+//Object Ex:-
+//Everything inside the curly braces is object
+let obj={
+    name:"vikash",
+    roll:24
+}
